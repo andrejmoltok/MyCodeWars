0db3504ed12e31ea3cae9d2724823b6ba82e07ac
Csokán Pál András <csokan.andras87@protonmail.ch>
2022-08-18 14:32:41 +0200

    has two solutions

---
+++ sumMixedArray.js
@@ -0,0 +1,20 @@
+function sumMix(x){
+
+    //Solution 1
+
+    let sum = 0;
+    for (let i = 0; i < x.length; i += 1) {
+        if (typeof x[i] !== 'number') {
+            x.splice(i,1,Number(x[i]));
+        }
+        sum += x[i];
+    }
+    return sum;
+
+    //Solution 2
+
+    return x.reduce((a,b) => Number(a) + Number(b));
+    
+  }
+
+  console.log(sumMix([9,3,'7','3']));
