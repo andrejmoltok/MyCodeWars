2fb7b5e79b793fdbd989c133d53193c684c17809
Csokán Pál András <csokan.andras87@protonmail.ch>
2022-08-18 13:49:52 +0200

    splitStrings solution

---
+++ splitStrings.js
@@ -0,0 +1,11 @@
+function solution(str) {
+    const len = str.length;
+    if (len % 2 !== 0) {
+        let arr = str.match(/.{1,2}/g);
+        arr.push(arr.pop() + '_');
+        return arr;
+    }
+    return str.match(/.{1,2}/g);
+}
+
+console.log(solution('abcdefg'));
