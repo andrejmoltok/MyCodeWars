6c38f7c434119204f5adc2d28d006bf35609c855
Csokán Pál András <csokan.andras87@protonmail.ch>
2022-08-18 20:20:44 +0200

    counting steps until single digit with recursion

---
+++ persistent.js
@@ -0,0 +1,16 @@
+function solution(num) {
+    let arr = Array.from(String(num),Number);
+    let count = 0;
+    if (arr.length > 1) {
+        let prod = arr.reduce((a,b) => a*b,1);
+        count += 1;
+        return count + solution(prod);
+    } else {
+        return 0;
+    }
+}
+
+console.log(solution(25));
+console.log(solution(39));
+console.log(solution(4));
+console.log(solution(999));
