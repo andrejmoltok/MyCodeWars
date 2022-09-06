1d8495d5c305b063bf4bed4aa9dba15d9000cd85
Csokán Pál András <csokan.andras87@protonmail.ch>
2022-09-06 19:51:31 +0200

    simple maths and if statement

---
+++ twiceAsOld.js
@@ -0,0 +1,11 @@
+function twiceAsOld(dadYearsOld, sonYearsOld) {
+    let twice = sonYearsOld * 2;
+    if (twice >= dadYearsOld) {
+        return twice - dadYearsOld;
+    } else {
+        return dadYearsOld - twice;
+    }
+}
+
+console.log(twiceAsOld(36,7));
+console.log(twiceAsOld(55,30));
