1fc11c42a7c1dd4a6b640e66a85987f5752baed0
Csokán Pál András <csokan.andras87@protonmail.ch>
2022-10-24 20:27:50 +0200

    Object Oriented Piracy solution

---
+++ OOPiracy.js
@@ -0,0 +1,14 @@
+function Ship(draft,crew) {
+    this.draft = draft;
+    this.crew = crew;
+}
+   
+Ship.prototype.isWorthIt = function() {
+    return (this.draft - (this.crew * 1.5)) > 20 ? true : false;
+}
+
+let aWorthyShip = new Ship(100,20);
+let emptyShip = new Ship(0,0);
+
+console.log(aWorthyShip.isWorthIt()); // true
+console.log(emptyShip.isWorthIt()); // false
