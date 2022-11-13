e5ff63888f8eff7a27833721879ccff85b8fc1f8
Csokán Pál András <csokan.andras87@protonmail.ch>
2022-11-13 09:46:08 +0100

    spoonerize solution

---
+++ spooner.js
@@ -0,0 +1,9 @@
+function spoonerize(words) {
+    let temp = words.split(' ')[0].charAt(0);
+    let out = [];
+    out.push(words.split(' ')[0].replace(temp,words.split(' ')[1].charAt(0)), 
+            words.split(' ')[1].replace(words.split(' ')[1].charAt(0),temp));
+    return out.join(' ');
+}
+
+console.log(spoonerize("nit picking"));
